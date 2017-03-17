b5f6c8cb33552587a4126d5122f061a99beac2b9
vrto <michal.vrtiak+github@gmail.com>
2017-03-17 19:14:31 +0100

    changing GET to POST and logging out whatever is sent to find out Slack request format

--- index.js
+++ index.js
@@ -2,29 +2,34 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const API_KEY = '633be368f95913639382d698b33f953b';
 
-app.get('/', function (req, res) {
-    const CITY = req.query.text;
-    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`;
-
-    axios.get(URL)
-    .then(function (response) {
-        const wdata = response.data;
-        res.json({
-            text: `The weather in ${wdata.name} (${wdata.sys.country})`,
-            attachments: [
-                {
-                    title: wdata.weather[0].description,
-                    text: `The current temperature is ${wdata.main.temp} (Low: ${wdata.main.temp_min} High: ${wdata.main.temp_max})`,
-                    thumb_url: `http://openweathermap.org/img/w/${wdata.weather[0].icon}.png`
-                }
-            ]
-        })
-    })
-    .catch(function (response) {
-        res.json(response)
-    });
+app.post('/', function (req, res) {
+    console.log(req.body);
+    // const CITY = req.query.text;
+    // const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`;
+    //
+    // axios.get(URL)
+    // .then(function (response) {
+    //     const wdata = response.data;
+    //     res.json({
+    //         text: `The weather in ${wdata.name} (${wdata.sys.country})`,
+    //         attachments: [
+    //             {
+    //                 title: wdata.weather[0].description,
+    //                 text: `The current temperature is ${wdata.main.temp} (Low: ${wdata.main.temp_min} High: ${wdata.main.temp_max})`,
+    //                 thumb_url: `http://openweathermap.org/img/w/${wdata.weather[0].icon}.png`
+    //             }
+    //         ]
+    //     })
+    // })
+    // .catch(function (response) {
+    //     res.json(response)
+    // });
 });
 
 app.set('port', (process.env.PORT || 5000));
