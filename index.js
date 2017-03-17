372bfe5694d325573e0871328af0af947f5ae726
vrto <michal.vrtiak+github@gmail.com>
2017-03-17 19:20:04 +0100

    Weather lookup is done using body.text form param

--- index.js
+++ index.js
@@ -9,27 +9,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const API_KEY = '633be368f95913639382d698b33f953b';
 
 app.post('/', function (req, res) {
-    console.log(req.body);
-    // const CITY = req.query.text;
-    // const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`;
-    //
-    // axios.get(URL)
-    // .then(function (response) {
-    //     const wdata = response.data;
-    //     res.json({
-    //         text: `The weather in ${wdata.name} (${wdata.sys.country})`,
-    //         attachments: [
-    //             {
-    //                 title: wdata.weather[0].description,
-    //                 text: `The current temperature is ${wdata.main.temp} (Low: ${wdata.main.temp_min} High: ${wdata.main.temp_max})`,
-    //                 thumb_url: `http://openweathermap.org/img/w/${wdata.weather[0].icon}.png`
-    //             }
-    //         ]
-    //     })
-    // })
-    // .catch(function (response) {
-    //     res.json(response)
-    // });
+    const CITY = req.body.text;
+    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`;
+
+    axios.get(URL)
+    .then(function (response) {
+        const wdata = response.data;
+        res.json({
+            text: `The weather in ${wdata.name} (${wdata.sys.country})`,
+            attachments: [
+                {
+                    title: wdata.weather[0].description,
+                    text: `The current temperature is ${wdata.main.temp} (Low: ${wdata.main.temp_min} High: ${wdata.main.temp_max})`,
+                    thumb_url: `http://openweathermap.org/img/w/${wdata.weather[0].icon}.png`
+                }
+            ]
+        })
+    })
+    .catch(function (response) {
+        res.json(response)
+    });
 });
 
 app.set('port', (process.env.PORT || 5000));
